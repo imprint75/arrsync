7792f1eac5e8189c4476e8fab36d57e63a8bb065
sean smith <seansmith@seans-MacBook-Pro-2.local>
2014-08-25 17:30:11 +0300

    Initial Commit

---
+++ ArrSync/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ArrSync
+//
+//  Created by sean smith on 8/25/14.
+//  Copyright (c) 2014 ssjc. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ArrSync/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  ArrSync
+//
+//  Created by sean smith on 8/25/14.
+//  Copyright (c) 2014 ssjc. All rights reserved.
+//
+
+import Cocoa
+
+NSApplicationMain(C_ARGC, C_ARGV)
